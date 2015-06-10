7cc8f320aac98a1e60f7fffdfc918248a885ec88
bethouxa <antonin.bethoux@etu.upmf-grenoble.fr>
2015-06-10 14:28:16 +0200

    cartes réimplémentées selon UML, reste à créer cartes CdC.

--- src/jeu/CarteMouvement.java
+++ src/jeu/CarteMouvement.java
@@ -3,29 +3,45 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package jeu;
 
 /**
  *
  * @author laugiera
  */
-public class CarteMouvement extends Cartes {
+public class CarteMouvement extends Carte {
 
-    private Carreau caseCible;
-            
-    public CarteMouvement(Monopoly mono) {
-        super(mono);
+    private int caseCible;
+    private boolean relatif;
+    /**
+     * 
+     * @param mono instance de monopoly
+     * @param numero numéro de la carte
+     * @param message massage inscrit sur la carte
+     * @param caseCible exacly what it says on the can
+     * @param relatif vrai si la caseCible est relative au joueur
+     */
+    public CarteMouvement(Monopoly mono, int numero, String message, int caseCible, boolean relatif) {
+        super(mono,numero,message);
+        this.caseCible = caseCible;
+        this.relatif = relatif;
     }
 
-    public Carreau getCaseCible() {
+    public int getCaseCible() {
         return caseCible;
     }
-
-    public void setCaseCible(Carreau caseCible) {
-        this.caseCible = caseCible;
+    
+    @Override
+    public void action(Joueur j) {
+        super.getMonopoly().interfaceJeu.messageCarte(this);
+        
+        if (relatif)
+            caseCible = (j.getPositionCourante()+caseCible)%40 +1;
+        
+        if (caseCible < j.getPositionCourante() && caseCible != 1) // Passage par la case départ
+            j.ajouterCash(200);
+        
+        j.deplacer(caseCible);
+        getMonopoly().getCarreau(j.getPositionCourante()).action(j);
     }
-    
-    
-    
 }
